9eaac49580a502f9f434a723530cfa29629b3f60
Wouter de Bruijn <wouter@hedium.nl>
2023-02-06 23:42:40 +0100

    🔥 Removed mod.ts, using index.ts

--- mod.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2020-2021 the AloeDB authors. All rights reserved. MIT license.
-
-export { Database } from './lib/database.ts';
-export * from './lib/helpers.ts';
-export * from './lib/types.ts';
